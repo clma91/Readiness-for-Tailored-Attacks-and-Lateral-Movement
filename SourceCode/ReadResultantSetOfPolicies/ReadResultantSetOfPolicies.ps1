5e7d3c84d39dcac14d5805e2fde375f47e2a9143
Claudio Mattes <claudio.mattes@bluewin.ch>
2018-10-28 15:08:05 +0100

    Kommentar entfernt

--- SourceCode/ReadResultantSetOfPolicies/ReadResultantSetOfPolicies.ps1
+++ SourceCode/ReadResultantSetOfPolicies/ReadResultantSetOfPolicies.ps1
@@ -4,8 +4,6 @@ function readResultantSetOfPolicies {
     $now=get-date
     $currentPath = (Resolve-Path .\).Path
     $pathRSOPXML = $currentPath + "\LocalUserAndComputerReport.xml"
-    $exportFile = $currentPath + "eventids" + $now.ToString("yyyy-MM-dd---hh-mm-ss") + ".csv"
-
     enum AuditSettingValues {
         NoAuditing
         Success
@@ -15,7 +13,7 @@ function readResultantSetOfPolicies {
     [AuditSettingValues]$auditSettingValue = 0;
     $auditSettingSubcategoryNames = @("Audit Sensitive Privilege Use","Audit Kerberos Service Ticket Operations","Audit Registry","Audit Security Group Management","Audit File System","Audit Process Termination","Audit Logoff","Audit Process Creation","Audit Filtering Platform Connection","Audit File Share","Audit Kernel Object","Audit MPSSVC Rule-Level Policy Change","Audit Non Sensitive Privilege Use","Audit Logon","Audit SAM","Audit Handle Manipulation","Audit Special Logon","Audit Detailed File Share","Audit Kerberos Authentication Service","Audit User Account Management")
 
-    # Get-GPResultantSetOfPolicy -ReportType Xml -Path  $pathRSOPXML | Out-Null;
+    Get-GPResultantSetOfPolicy -ReportType Xml -Path  $pathRSOPXML | Out-Null;
     [xml]$rsopResult = Get-Content $pathRSOPXML;
 
     $auditSettings = $rsopResult.Rsop.ComputerResults.ExtensionData.Extension.AuditSetting
