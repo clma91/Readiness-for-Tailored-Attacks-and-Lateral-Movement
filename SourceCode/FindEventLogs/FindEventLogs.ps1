f23fcbdc958d10e5a59eb8a2dc75506aafbe8a01
mrchelli <lukas.kellenberger@hsr.ch>
2018-10-31 08:17:08 +0100

    Meeting KW44

--- SourceCode/FindEventLogs/FindEventLogs.ps1
+++ SourceCode/FindEventLogs/FindEventLogs.ps1
@@ -6,7 +6,6 @@ Set-Variable -Name CheckApplicationLogIDs -Value @(6, 21, 24, 102, 106, 129, 169
 Set-Variable -Name CheckSystemLogIDs -Value @(7036, 7045, 20001)#according to JPCERT
 Set-Variable -Name CheckSecurityLogIDs -Value @(104, 4624, 4634, 4648, 4656, 4658, 4660, 4661, 4663, 4672, 4673, 4688, 4689, 4690, 4720, 4726, 4728, 4729, 4768, 4769, 4946, 5140, 5142, 5144, 5145, 5154, 5156, 8222)#according to JPCERT
 
-#TODO: Function mit zwei Parameter um Duplicated Code zu vermeiden
 $now=get-date
 $ExportFile=$ExportFolder + "\eventids" + $now.ToString("yyyy-MM-dd---hh-mm-ss") + ".txt"
 $events = New-Object System.Collections.ArrayList
