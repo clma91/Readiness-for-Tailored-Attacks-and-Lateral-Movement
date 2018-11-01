16f0a42d1ed8121976826f2c85017ae120eb7340
Claudio Mattes <claudio.mattes@bluewin.ch>
2018-11-01 16:35:57 +0100

    corrected typo

--- SourceCode/RunPester.ps1
+++ SourceCode/RunPester.ps1
@@ -1,2 +1,2 @@
 Import-Module "$PSScriptRoot\Pester\Pester.psm1"  
-Invoke-Pester -EnableExit -OutputFile "./artifacts/TestResults.xml" -OutputFormat NUnitXm
+Invoke-Pester -EnableExit -OutputFile "./artifacts/TestResults.xml" -OutputFormat NUnitXML
