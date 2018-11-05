92212494482c337e7c7a4d180dc7b7c594c05c25
Claudio Mattes <claudio.mattes@bluewin.ch>
2018-11-05 10:18:55 +0100

    changed Invoke-Pester

--- SourceCode/RunPester.ps1
+++ SourceCode/RunPester.ps1
@@ -1,2 +1,2 @@
 Import-Module "$PSScriptRoot\Pester\Pester.psm1"  
-Invoke-Pester -CodeCoverage *.ps1 -Path "SourceCode/TestResults.xml" -OutputFormat NUnitXml -OutputFile "$PSScriptRoot\TestResult.xml"
+Invoke-Pester -EnableExit -OutputFile "SourceCode/TestResults.xml" -OutputFormat NUnitXML
