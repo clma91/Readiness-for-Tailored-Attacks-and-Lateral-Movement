76a3feec17fb94008a9ee6d018fa1a3ad1020dd0
Claudio Mattes <claudio.mattes@bluewin.ch>
2018-11-08 13:12:39 +0100

    changed path in RunPetster.ps1

--- SourceCode/RunPester.ps1
+++ SourceCode/RunPester.ps1
@@ -1,3 +1,2 @@
 Import-Module "$PSScriptRoot\Pester\Pester.psm1"  
-# Invoke-Pester -EnableExit -OutputFile "$PSScriptRoot\TestResults.xml" -OutputFormat NUnitXML
-Invoke-Pester -Script "SourceCode\SRI" -OutputFormat NUnitXml -OutputFile "SourceCode\TestResults.xml" -PassThru -ExcludeTag Incomplete
+Invoke-Pester -Script "$PSScriptRoot\SRI" -OutputFormat NUnitXml -OutputFile "$PSScriptRoot\TestResults.xml" -PassThru -ExcludeTag Incomplete
