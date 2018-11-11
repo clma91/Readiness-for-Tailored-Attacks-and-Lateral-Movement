0e493cff9556609fc889e62d6cb2093f6431d59d
Claudio Mattes <claudio.mattes@bluewin.ch>
2018-11-11 16:30:39 +0100

    added local and azure path

--- SourceCode/SRI/ReadResultantSetOfPolicies/GetAndAnalyseAuditPolicies.Tests.ps1
+++ SourceCode/SRI/ReadResultantSetOfPolicies/GetAndAnalyseAuditPolicies.Tests.ps1
@@ -1,6 +1,6 @@
 # Pester tests
 $localPath = (Resolve-Path .\).Path
-$azurePath = $currentPath + "\SourceCode\SRI\ReadResultantSetOfPolicies"
+$azurePath = $localPath + "\SourceCode\SRI\ReadResultantSetOfPolicies"
 $currentPath = $azurePath
 $modulePath = $currentPath + "\GetAndAnalyseAuditPolicies.psm1"
 Import-Module $modulePath -Force
