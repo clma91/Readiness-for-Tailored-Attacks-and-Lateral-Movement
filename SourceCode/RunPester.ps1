5f0384d9bf8db8782161db7f46dc6b87f0fc7bde
Claudio Mattes <claudio.mattes@bluewin.ch>
2018-11-11 15:43:57 +0100

    changed RunPester

--- SourceCode/RunPester.ps1
+++ SourceCode/RunPester.ps1
@@ -1,2 +1,3 @@
 Import-Module "$PSScriptRoot\Pester\Pester.psm1"  
-Invoke-Pester -Script "$PSScriptRoot\SRI", "$PSScriptRoot\Trial" -OutputFormat NUnitXml -OutputFile "$PSScriptRoot\TestResults.xml" -PassThru -ExcludeTag Incomplete
+Invoke-Pester -EnableExit -OutputFormat NUnitXml -OutputFile "$PSScriptRoot\TestResults.xml" -PassThru -ExcludeTag Incomplete
+# Invoke-Pester -Script "$PSScriptRoot\SRI", "$PSScriptRoot\Trial" -OutputFormat NUnitXml -OutputFile "$PSScriptRoot\TestResults.xml" -PassThru -ExcludeTag Incomplete
